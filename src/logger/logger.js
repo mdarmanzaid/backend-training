209bcd1faff7ebfd5f208578483a2a0e6e092230
mdarmanzaid <you@example.com>
2022-08-06 01:29:07 +0530

    updated file

---
+++ src/logger/logger.js
@@ -0,0 +1,10 @@
+// - welcome() -> prints ‘Welcome to my application. I am <name> 
+// and a part of FunctionUp Plutonium cohort.’ on console
+
+const xyz = " Welcome to my application. I am Md Arman and a part of FunctionUp Plutonium cohort"
+let fun  = function() {
+
+}
+console.log(xyz)
+
+module.exports.ab = xyz

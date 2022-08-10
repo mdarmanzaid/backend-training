8956d5e2b67cd4f042b67183696006efb9ab9ace
mdarmanzaid <you@example.com>
2022-08-10 22:51:43 +0530

    updated on 10 august

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 var bodyParser = require('body-parser');
 
 const route = require('./routes/route.js');
+const router = require('./routes/route.js');
 
 const app = express();
 
@@ -13,3 +14,57 @@ app.use('/', route);
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
+
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ]
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ]
+       },
+   ]
+
+
+router.post('/players',function(req,res){
+
+let newPlayer =req.body
+let newPlayersName=newPlayer
+let isNameRepeated=false
+
+for(let i=0;i<newPlayer.length;i++){
+    if(players[i].name==newPlayersName){
+        isNameRepeated=true
+        break;
+    }
+}
+if(isNameRepeated){
+res.send ("This player was already added")
+
+}else{
+    players.push(newPlayer)
+    res.send({data :players, staus:true} )
+}
+})
+module.exports=router;
